b173d1224803770cdb8fe210cf2c65b2730373a2
minhtungo <loducfc@gmail.com>
2022-08-28 02:50:00 -0400

    set up sanity, schemas and fetchCategories

---
+++ pages/api/getCategories.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { groq } from 'next-sanity';
+import { sanityClient } from '../../sanity';
+
+type Data = {
+  categories: Category[];
+};
+
+const query = groq`*[_type == "category"] {
+_id,
+  ...
+}`;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const categories = await sanityClient.fetch(query);
+  res.status(200).json({ categories });
+}
